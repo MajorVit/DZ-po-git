95f44e88d3d617850e18e86f0f711479a8d30f16
19733726 <vitaliy_mayorskiy@mail.ru>
2022-08-12 00:38:27 +0300

    add DZ_git.java

--- src/test/java/DZ_git.java
+++ src/test/java/DZ_git.java
@@ -1,7 +1,7 @@
 public class DZ_git {
 
         public static void main(String[] args) {
-            System.out.println("Dogs are better than cats!");
+            System.out.println("Cats are better than dogs!");
         }
     }
 
