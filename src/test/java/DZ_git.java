5b0b6053fd6aba64e883beb4c4b1d29d0da6289e
19733726 <vitaliy_mayorskiy@mail.ru>
2022-08-12 01:15:14 +0300

    = <3

--- src/test/java/DZ_git.java
+++ src/test/java/DZ_git.java
@@ -1,7 +1,7 @@
 public class DZ_git {
 
         public static void main(String[] args) {
-            System.out.println("Dogs and cats one love!");
+            System.out.println("Dogs and cats one love");
         }
     }
 
